3c03a3bd387f76ac605ade27636c0ec46ada1e1e
Marcus Molchany <marcusmolchany@gmail.com>
2022-02-01 21:06:14 -0800

    export signMessage

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { tokenGate } from "./tokenGate";
 
+import { signMessage, signTypedData } from "./signature";
+
 export const insecureClientSideTokenGate = tokenGate;
+export const clientSideSignMessage = signMessage;
+export const clientSideSignTypedData = signTypedData; /* todo */
 export const secureServerSideTokenGate = undefined; /* todo */
