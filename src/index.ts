393fe3595d8f15f466db34e3c4e3b7e6b3ab17d1
Marcus Molchany <marcusmolchany@gmail.com>
2022-02-01 21:17:41 -0800

    export server side token gate

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { tokenGate } from "./tokenGate";
+import { tokenGate, recoverAddressAndCheckTokenGate } from "./tokenGate";
 
 import { signMessage, signTypedData } from "./signature";
 
 export const insecureClientSideTokenGate = tokenGate;
 export const clientSideSignMessage = signMessage;
 export const clientSideSignTypedData = signTypedData; /* todo */
-export const secureServerSideTokenGate = undefined; /* todo */
+export const secureServerSideTokenGate = recoverAddressAndCheckTokenGate;
